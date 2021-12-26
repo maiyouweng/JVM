1a51007997cc7e5fd94d05da8c413004dd2b4dc6
jy03 <lixiaoyongbit@163.com>
2021-12-26 16:26:52 +0800

    002

--- RunDataArea/src/MethodArea/OOMTest.java
+++ RunDataArea/src/MethodArea/OOMTest.java
@@ -2,6 +2,6 @@ package MethodArea;
 
 public class OOMTest {
     public static void main(String[] args) {
-        System.out.println("This is a class");
+        System.out.println("This is a class");//
     }
 }
